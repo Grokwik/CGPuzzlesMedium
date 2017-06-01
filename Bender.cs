79b0a913233be8ebb163d62a843cd8dd57fe8f8f
Grokwik <frederic@bodin.site>
2017-06-01 22:52:25 +0200

    Bender @100% :)
    Hell to the yeah !
    No more medium puzzles !!

--- Bender.cs
+++ Bender.cs
@@ -1,5 +1,14 @@
 using System;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
+
+/*
+south = 0
+east = 1
+north = 2
+west = 3
+*/
 
 struct Pos
 {
@@ -7,249 +16,254 @@ struct Pos
     public int y;
 }
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Solution
 {
-    #region     Properties
-    static string           output;
-    static int              L, C;
-    static List<List<int>>  histoGrid;  //  keep the invert/drunk/direction states for each cell
-    static List<string>     grid;       //  Map
-    static List<char>       blockers;
-    static string           dir;        //  Bender's direction
-    static Pos              p, teleport1, teleport2;
-    static bool             Drunk, Invert;
+    #region Properties
+    static string Dir;                  //  Bender's direction
+    static int L, C, CurrentDir;
+    static List<List<int>> HistoGrid;   //  keep the invert/drunk/direction states for each cell
+    static List<string> Grid;           //  Map
+    static List<char> Blockers;
+    static String[] strDirs;
+    static Pos[] PosInc;
+    static Pos P, Teleport1, Teleport2;
+    static bool Drunk, Invert, Finished;
+    static Queue<string> StairwayToHell;
     #endregion
 
-    static void reinitHistoGrid()
+    static void WhereAmI()
     {
-        histoGrid = new List<List<int>>();
-        for(var l=0 ; l<L ; l++)
+        switch (Grid[P.y][P.x])
         {
-            var line = new List<int>();
-            for (var i=0 ; i<C ; i++)
-                line.Add(0);
-            histoGrid.Add(line);
+            case '$': Finished = true;
+                      Console.Error.WriteLine("WhereAmI: Cheguei !");
+                      break;
+            case 'N': CurrentDir = 2;
+                      Console.Error.WriteLine("WhereAmI: Let's head North");
+                      break;
+            case 'S': CurrentDir = 0;
+                      Console.Error.WriteLine("WhereAmI: Let's head South");
+                      break;
+            case 'W': CurrentDir = 3;
+                      Console.Error.WriteLine("WhereAmI: Let's head West");
+                      break;
+            case 'E': CurrentDir = 1;
+                      Console.Error.WriteLine("WhereAmI: Let's head East");
+                      break;
+            case 'B': Drunk = !Drunk;
+                      UpdateBlockers();
+                      Console.Error.WriteLine("WhereAmI: Yeeah, beer time !");
+                      break;
+            case 'I': Invert = !Invert;
+                      Console.Error.WriteLine("WhereAmI: White rabbit mode");
+                      break;
+            case 'T': if (P.x == Teleport1.x && P.y == Teleport1.y)
+                      {
+                          P.x = Teleport2.x;
+                          P.y = Teleport2.y;
+                      }
+                      else
+                      {
+                          P.x = Teleport1.x;
+                          P.y = Teleport1.y;
+                      }
+                      Console.Error.WriteLine("WhereAmI: Scotty !! Teleport !!");
+                      break;
+            case 'X': Console.Error.WriteLine("WhereAmI: Mode passe muraille");
+                      Console.Error.WriteLine(Grid[P.y]);
+                      var newLine = Grid[P.y].Substring(0, P.x);
+                      newLine += " ";
+                      newLine += Grid[P.y].Substring(P.x+1);
+                      Grid[P.y] = newLine;
+                      Console.Error.WriteLine(Grid[P.y]);
+                      ReinitHistoGrid();
+                      break;
         }
     }
 
-    static bool registerHisto()
+    static void WhereDoIGo()
     {
-        int state = 0;
-        var filter = 0;
-        if (dir == "WEST")
-        {
-            filter = 7;             //  000 000 000 111
-            state += 1;             //  000 000 000 001
-            if (Invert) state += 2; //  000 000 000 010
-            if (Drunk) state += 4;  //  000 000 000 100
-        }
-        else if (dir == "EAST")
-        {
-            filter = 56;            //  000 000 111 000
-            state += 8;             //  000 000 001 000
-            if (Invert) state += 16;//  000 000 010 000
-            if (Drunk) state += 32; //  000 000 100 000
-        }
-        else if (dir == "SOUTH")
-        {
-            filter = 448;            //  000 111 000 000
-            state += 64;             //  000 001 000 000
-            if (Invert) state += 128;//  000 010 000 000
-            if (Drunk) state += 256; //  000 100 000 000
-        }
-        else if (dir == "NORTH")
-        {
-            filter = 3584;              //  111 000 000 000
-            state += 512;               //  001 000 000 000
-              if (Invert) state += 1024;//  010 000 000 000
-            if (Drunk) state += 2048;   //  100 000 000 000
-        }
-        if ((histoGrid[p.y][p.x] & filter) == state)
-            return false;
+        char[] cells = new char[4];
+        cells[0] = Grid[P.y + 1][P.x]; // south
+        cells[1] = Grid[P.y][P.x + 1]; // east
+        cells[2] = Grid[P.y - 1][P.x]; // north
+        cells[3] = Grid[P.y][P.x - 1]; // west
 
-        histoGrid[p.y][p.x] |= state;
-        return true;
-    }
-    
-    static Pos getDelta(string newdir)
-    {
-        Pos delta;
-        delta.x = 0;
-        delta.y = 0;
-        if     (newdir == "NORTH")  delta.y = -1;
-        else if(newdir == "SOUTH")  delta.y = 1;
-        else if(newdir == "EAST")   delta.x = 1;
-        else if(newdir == "WEST")   delta.x = -1;
-        return delta;
-    }
-
-    static bool specialCasesD(char modifier, Pos delta)
-    {
-        if (!Drunk)
-            return false;
-
-        switch(modifier)
+        if (!Blockers.Contains(cells[CurrentDir]))
         {
-            case 'X':   string newLine = "";
-                        newLine = grid[p.y+delta.y].Substring(0, p.x+delta.x);
-                        newLine += " ";
-                        newLine += grid[p.y+delta.y].Substring(p.x+delta.x+1);
-                        grid[p.y+delta.y] = newLine;
-                        reinitHistoGrid();
-                        return true;
-            case 'B':   Drunk = false;
-                        return true;
+            Dir = strDirs[CurrentDir];
+            StairwayToHell.Enqueue(Dir);
+            P.x += PosInc[CurrentDir].x;
+            P.y += PosInc[CurrentDir].y;
+            return;
         }
-        return false;
-    }
-
-    static bool specialCasesI(char modifier, ref Pos delta, ref string newdir)
-    {
-        if (!Invert)
-            return false;
-
-        switch(modifier)
-        {
-            case 'X':
-            case '#':   if      (!blockers.Contains(grid[p.y][p.x-1]))  newdir = "WEST";
-                        else if (!blockers.Contains(grid[p.y-1][p.x]))  newdir = "NORTH";
-                        else if (!blockers.Contains(grid[p.y][p.x+1]))  newdir = "EAST";
-                        else                                            newdir = "SOUTH";
-                        dir = newdir;
-                        delta = getDelta(newdir);
-                        return true;
-            case 'I':   Invert = false;
-                        return true;
-        }
-        return false;
-    }
-
-    static bool findModifierN()
-    {
-        string  newdir = dir;
-        Pos delta = getDelta(dir);
-        char modifier = grid[p.y+delta.y][p.x+delta.x];
-
-        bool alreadyDone = specialCasesD(modifier, delta);
-        if (alreadyDone)
-            modifier = grid[p.y+delta.y][p.x+delta.x];
-        alreadyDone |= specialCasesI(modifier, ref delta, ref newdir);
-        if (!alreadyDone)
-        {
-            switch(modifier)
-            {
-                case '$': return false;
-                case 'X': 
-                case '#':
-                        if      (!blockers.Contains(grid[p.y+1][p.x]))  dir = "SOUTH";
-                        else if (!blockers.Contains(grid[p.y][p.x+1]))  dir = "EAST";
-                        else if (!blockers.Contains(grid[p.y-1][p.x]))  dir = "NORTH";
-                        else                                            dir = "WEST";
-                        delta = getDelta(dir);
-                        newdir = dir;
-                        return true;
-                case 'N': if (!Invert) newdir = "NORTH"; break;
-                case 'S': if (!Invert) newdir = "SOUTH"; break;
-                case 'E': if (!Invert) newdir = "EAST";  break;
-                case 'W': if (!Invert) newdir = "WEST";  break;
-                case 'B': if (!Drunk)  Drunk = true;     break;
-                case 'I': if (!Invert) Invert = true;    break;
-                case 'T':
-                    if (teleport1.x == (p.x+delta.x)
-                     && teleport1.y == (p.y+delta.y))
-                    {
-                        delta.x = teleport2.x-p.x;
-                        delta.y = teleport2.y-p.y;
-                    }
-                    else
-                    {
-                        delta.x = teleport2.x-p.x;
-                        delta.y = teleport1.y-p.y;
-                    }
-                    break;
-            }
-        }
-        p.x += delta.x;
-        p.y += delta.y;
         
-        if (!registerHisto())
+        int scannedDir;
+        scannedDir = (Invert) ? 3 : 0;
+        while (scannedDir<4 && scannedDir>=0)
         {
-            output = "LOOP";
-            return false;
+            if (!Blockers.Contains(cells[scannedDir]))
+            {
+                Dir = strDirs[scannedDir];
+                StairwayToHell.Enqueue(Dir);
+                CurrentDir = scannedDir;
+                P.x += PosInc[scannedDir].x;
+                P.y += PosInc[scannedDir].y;
+                break;
+            }
+            if (Invert)
+                scannedDir--;
+            else
+                scannedDir++;
         }
-        output += dir+"\n"; // ----------
-        dir = newdir;
-        return true;
+    }
+
+    static void UpdateBlockers()
+    {
+        Blockers = new List<char>() { '#' };
+        if (!Drunk)
+        {
+            Blockers.Add('X');
+        }
     }
 
     static void Main(string[] args)
     {
         #region Init
-        output = "";
-        grid = new List<string>();
-        blockers = new List<char>() {'#', 'X'};
+        Grid = new List<string>();
+        StairwayToHell = new Queue<string>();
         var inputs = Console.ReadLine().Split(' ');
         L = int.Parse(inputs[0]);
         C = int.Parse(inputs[1]);
-        
-        Console.Error.WriteLine("{0} {1}", L, C);
+        UpdateBlockers();
+        PosInc = new Pos[4];
+        PosInc[0].y = 1;  // south
+        PosInc[1].x = 1;  // east
+        PosInc[2].y = -1; // north
+        PosInc[3].x = -1; // west
 
-        p.x=0;
-        p.y=0; 
+        strDirs = new String[4];
+        strDirs[0] = "SOUTH";
+        strDirs[1] = "EAST";
+        strDirs[2] = "NORTH";
+        strDirs[3] = "WEST";
+
         Drunk = false;
         Invert = false;
-        dir = "SOUTH";
+        Dir = "SOUTH";
+        Finished = false;
+
         for (int l = 0; l < L; l++)
         {
             string row = Console.ReadLine();
-            grid.Add(row);
+            Grid.Add(row);
 
             //  Look for the '@'
             var aropos = row.IndexOf("@");
             if (aropos != -1)
             {
-                p.x = aropos;
-                p.y = l;
+                P.x = aropos;
+                P.y = l;
             }
 
             //  Look for the teleporters
             var Tidx = row.IndexOf("T");
             if (Tidx != -1)
             {
-                if (teleport1.x==0)
+                if (Teleport1.x == 0)
                 {
-                    teleport1.x = Tidx;
-                    teleport1.y = l;
+                    Teleport1.x = Tidx;
+                    Teleport1.y = l;
                 }
                 else
                 {
-                    teleport2.x = Tidx;
-                    teleport2.y = l;
+                    Teleport2.x = Tidx;
+                    Teleport2.y = l;
                 }
             }
         }
-        reinitHistoGrid();
+        CurrentDir = 0; // 0 = SOUTH
+
+        ReinitHistoGrid();
         #endregion
 
-        #region Debug info
-        Console.Error.WriteLine("Initial pos="+p.x+","+p.y);
+        DisplayGrid();
+
+        while (!Finished)
+        {
+            WhereDoIGo();
+            WhereAmI();
+            if (!RegisterHisto())
+            {
+                Console.WriteLine("LOOP");
+                Finished = true;
+                StairwayToHell.Clear();
+            }
+        }
+        while (0 != StairwayToHell.Count)
+        {
+            Dir = StairwayToHell.Dequeue();
+            Console.WriteLine(Dir);
+        }
+    }
+
+    static void DisplayGrid()
+    {
+        Console.Error.WriteLine("Initial pos=" + P.x + "," + P.y);
         for (int l = 0; l < L; l++)
         {
-            Console.Error.WriteLine(grid[l]);
+            Console.Error.WriteLine(Grid[l]);
         }
-        #endregion
+    }
 
-        bool exitCode = true;
-        while(exitCode)
+    static void ReinitHistoGrid()
+    {
+        HistoGrid = new List<List<int>>();
+        for (var l = 0; l < L; l++)
         {
-            exitCode = findModifierN();
+            var line = new List<int>();
+            for (var i = 0; i < C; i++)
+                line.Add(0);
+            HistoGrid.Add(line);
         }
-        if (output != "LOOP")
-            output += dir+"\n"; // ----------
-        Console.WriteLine(output);
+    }
+
+    static bool RegisterHisto()
+    {
+        int state = 0;
+        var filter = 0;
+        if (Dir == "WEST")
+        {
+            filter = 7;             //  000 000 000 111
+            state += 1;             //  000 000 000 001
+            if (Invert) state += 2; //  000 000 000 010
+            if (Drunk) state += 4;  //  000 000 000 100
+        }
+        else if (Dir == "EAST")
+        {
+            filter = 56;            //  000 000 111 000
+            state += 8;             //  000 000 001 000
+            if (Invert) state += 16;//  000 000 010 000
+            if (Drunk) state += 32; //  000 000 100 000
+        }
+        else if (Dir == "SOUTH")
+        {
+            filter = 448;            //  000 111 000 000
+            state += 64;             //  000 001 000 000
+            if (Invert) state += 128;//  000 010 000 000
+            if (Drunk) state += 256; //  000 100 000 000
+        }
+        else if (Dir == "NORTH")
+        {
+            filter = 3584;              //  111 000 000 000
+            state += 512;               //  001 000 000 000
+            if (Invert) state += 1024;  //  010 000 000 000
+            if (Drunk) state += 2048;   //  100 000 000 000
+        }
+        if ((HistoGrid[P.y][P.x] & filter) == state)
+            return false;
+
+        HistoGrid[P.y][P.x] |= state;
+        return true;
     }
 }
